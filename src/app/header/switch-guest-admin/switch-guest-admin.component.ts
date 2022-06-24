1d28d0198f07b9a099e2f820e38bcaa5eafa1bec
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-06-24 17:46:40 +0200

    [FEAT] Logical connection with user-admin-Service
    Insert new component in the Header (with guest / admin dynamic text).

--- src/app/header/switch-guest-admin/switch-guest-admin.component.ts
+++ src/app/header/switch-guest-admin/switch-guest-admin.component.ts
@@ -7,10 +7,26 @@ import { UserAdminService } from 'src/app/shared/user-admin.service';
   styleUrls: ['./switch-guest-admin.component.css'],
 })
 export class SwitchGuestAdminComponent implements OnInit {
-  switchText: string;
   isUserAdmin: boolean;
-  constructor(private userAdminService: UserAdminService) {}
+  switchText: string;
 
-  ngOnInit(): void {
+  constructor(private userAdminService: UserAdminService) {
+    this.isUserAdmin = this.userAdminService.isUserAdmin;
+    this.setSwitchText(this.userAdminService.isUserAdmin);
+  }
 
+  ngOnInit(): void {}
+
+  onSwitchUser() {
+    if (this.userAdminService.isUserAdmin) {
+      this.userAdminService.setUserGuest();
+      this.setSwitchText(false);
+    } else {
+      this.userAdminService.setUserAdmin();
+      this.setSwitchText(true);
+    }
+  }
+  setSwitchText(booleanValue: boolean) {
+    booleanValue ? (this.switchText = 'Admin') : (this.switchText = 'Guest');
+  }
 }
