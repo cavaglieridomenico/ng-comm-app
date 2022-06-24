178e0216b36644fe2aa41e85d922729abb0f1d72
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-06-24 18:48:32 +0200

    [FEAT] Static endpiont caption

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,21 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  currentUrl: any;
+  isYachtsPage: boolean;
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit(): void {
+    this.currentUrl = this.location.path();
+    console.log(this.currentUrl);
+    this.isYachtsPage = this.currentUrl === '/yachts';
+    console.log(this.isYachtsPage);
   }
-
 }
