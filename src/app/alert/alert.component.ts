4647344238b65724652c10771eecdc3965614284
Cristiano Franco <francocristo10@gmail.com>
2022-06-29 12:46:43 +0200

    [FEAT] Navbar sticky top on scrolling

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
 import { Alert } from './alert.model';
 import { AlertService } from './alert.service';
 
@@ -8,10 +9,22 @@ import { AlertService } from './alert.service';
   styleUrls: ['./alert.component.css'],
 })
 export class AlertComponent implements OnInit {
+  isYachtsPage: boolean;
+  currentRoute: string;
   alert: Alert;
-  constructor(private alertService: AlertService) {
+  constructor(private alertService: AlertService, private router: Router) {
     this.alert = this.alertService.getAlertData();
-  }
 
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+        if (this.currentRoute === '/yachts') {
+          this.isYachtsPage = true;
+        } else {
+          this.isYachtsPage = false;
+        }
+      }
+    });
+  }
   ngOnInit(): void {}
 }
